162a6d0534b30de608857aa5dbf40c94a1027240
Tushar Soni <tusharsoni1205@gmail.com>
2018-12-14 18:34:19 -0500

    Add cerror package for better error traces

---
+++ cerror/error.go
@@ -0,0 +1,44 @@
+package cerror
+
+import (
+	"fmt"
+	"strings"
+)
+
+type Error struct {
+	Message string
+	Tags    map[string]string
+	Cause   error
+}
+
+func New(cause error, msg string, tags map[string]string) error {
+	return &Error{
+		Message: msg,
+		Tags:    tags,
+		Cause:   cause,
+	}
+}
+
+func (e *Error) Error() string {
+	var err strings.Builder
+	var tags []string
+
+	err.WriteString(e.Message)
+
+	for tag, val := range e.Tags {
+		tags = append(tags, fmt.Sprintf("%s=%s", tag, val))
+	}
+
+	if len(tags) > 0 {
+		err.WriteString(" where ")
+		err.WriteString(strings.Join(tags, ","))
+	}
+
+	if e.Cause != nil {
+		err.WriteString(" because\n")
+		err.WriteString("> ")
+		err.WriteString(e.Cause.Error())
+	}
+
+	return err.String()
+}
