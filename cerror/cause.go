09e33b367f65af7ce2292f2ab866ee18bd8c7942
Tushar Soni <tusharsoni1205@gmail.com>
2018-12-16 14:39:30 -0500

    Add godoc for cerror package

--- cerror/cause.go
+++ cerror/cause.go
@@ -1,5 +1,7 @@
 package cerror
 
+// Cause returns the cause of the error as provided when the error was created using cerror.New.
+// Cause returns nil if the error has no given cause.
 func Cause(err error) error {
 	if cerr, ok := err.(Error); ok {
 		return cerr.Cause
--- cerror/error.go
+++ cerror/error.go
@@ -1,3 +1,5 @@
+// Package cerror provides a custom error type that can hold more context than the stdlib error package.
+// The goal is to provide better logging and debugging.
 package cerror
 
 import (
@@ -5,12 +7,15 @@ import (
 	"strings"
 )
 
+// Error is a custom error type that can hold tags and cause of an error for better debugging and logging.
 type Error struct {
 	Message string
 	Tags    map[string]string
 	Cause   error
 }
 
+// New is used to create a new Error with a cause, message, tags.
+// Cause and tags are optional and can be nil.
 func New(cause error, msg string, tags map[string]string) error {
 	return Error{
 		Message: msg,
@@ -19,6 +24,7 @@ func New(cause error, msg string, tags map[string]string) error {
 	}
 }
 
+// Error creates a log-friendly string of the error using the cause and tags.
 func (e Error) Error() string {
 	var err strings.Builder
 	var tags []string
