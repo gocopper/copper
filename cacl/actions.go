74a46ab2cb1bb9d54c78fd3c06a237751555843a
Tushar Soni <tusharsoni1205@gmail.com>
2019-03-03 13:08:30 -0500

    Add common acl actions for convenience

--- cacl/actions.go
+++ cacl/actions.go
@@ -5,3 +5,9 @@ const (
 	ActionRead  = "READ"
 	ActionWrite = "WRITE"
 )
+
+// Common combination of actions used for managing permission
+var (
+	ActionReadWrite = []string{ActionRead, ActionWrite}
+	ActionReadOnly  = []string{ActionRead}
+)
