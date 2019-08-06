efb05477948d299cd490251c6bf915a1e7d9e223
Tushar Soni <tusharsoni1205@gmail.com>
2019-08-05 23:14:57 -0400

    Add HasCause func to cerror

--- cerror/cause.go
+++ cerror/cause.go
@@ -8,3 +8,17 @@ func Cause(err error) error {
 	}
 	return nil
 }
+
+// HasCause returns whether the target error is in the error chain
+func HasCause(err, target error) bool {
+	cerr, ok := err.(Error)
+	if !ok {
+		return err.Error() == target.Error()
+	}
+
+	if cerr.Cause == nil {
+		return false
+	}
+
+	return HasCause(cerr.Cause, target)
+}
