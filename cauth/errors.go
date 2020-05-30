f57352c82e897ba1b6512f967f8d763e4e099702
Tushar Soni <tusharsoni1205@gmail.com>
2020-05-30 18:03:53 -0400

    [cauth] Fix issue where ErrUserNotFound was not defined

--- cauth/errors.go
+++ cauth/errors.go
@@ -1,6 +1,10 @@
 package cauth
 
-import "errors"
+import (
+	"errors"
+
+	"github.com/jinzhu/gorm"
+)
 
 // ErrUserAlreadyExists is returned by UsersSvc when a user already exists. For example, signing up with an email
 // with which a user already exists.
@@ -9,3 +13,6 @@ var ErrUserAlreadyExists = errors.New("user already exists")
 // ErrInvalidCredentials is returned by UsersSvc when the given credentials such as email/password or session token
 // are incorrect.
 var ErrInvalidCredentials = errors.New("invalid credentials")
+
+// ErrUserNotFound is returned when the user is not found when queried by a unique attribute such as id or email.
+var ErrUserNotFound = gorm.ErrRecordNotFound
