156051f211f91307aae41b4d9237940a88f5ffa2
Tushar Soni <tusharsoni1205@gmail.com>
2021-02-13 11:24:16 -0500

    Add tests for noop logger

--- v2/clogger/noop_test.go
+++ v2/clogger/noop_test.go
@@ -25,3 +25,35 @@ func TestNoopLogger_WithTags(t *testing.T) {
 
 	assert.NotNil(t, logger)
 }
+
+func TestNoopLogger_Debug(t *testing.T) {
+	t.Parallel()
+
+	logger := clogger.NewNoop().WithTags(nil)
+
+	logger.Debug("test-debug")
+}
+
+func TestNoopLogger_Info(t *testing.T) {
+	t.Parallel()
+
+	logger := clogger.NewNoop().WithTags(nil)
+
+	logger.Info("info")
+}
+
+func TestNoopLogger_Warn(t *testing.T) {
+	t.Parallel()
+
+	logger := clogger.NewNoop().WithTags(nil)
+
+	logger.Warn("warn", nil)
+}
+
+func TestNoopLogger_Error(t *testing.T) {
+	t.Parallel()
+
+	logger := clogger.NewNoop().WithTags(nil)
+
+	logger.Error("error", nil)
+}
