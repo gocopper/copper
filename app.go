669f0e82c406047723ead02983aafc9da85ce9c7
Tushar Soni <tusharsoni1205@gmail.com>
2021-08-28 12:56:27 -0400

    Set Start to always wait for exit signal

--- app.go
+++ app.go
@@ -37,8 +37,8 @@ type App struct {
 // Run runs the provided funcs. Once all of the functions complete their run,
 // the  lifecycle's stop funcs are also called. If any of the fns return an error,
 // the app exits with an exit code 1.
-// Run should be used when none of the fn are blocking. For blocking funcs like
-// a long running server, use Start.
+// Run should be used when none of the fn are long-running. For long-running funcs like
+// an HTTP server, use Start.
 func (a *App) Run(fns ...Runner) {
 	for i := range fns {
 		err := fns[i].Run()
@@ -52,23 +52,12 @@ func (a *App) Run(fns ...Runner) {
 	a.Lifecycle.Stop()
 }
 
-// Start runs the provided fns where the last provided fn be blocking like a
-// long running  server. The app listens on the OS's INT and TERM signals from the
+// Start runs the provided fns and then waits on the OS's INT and TERM signals from the
 // user to exit. Once the signal is received, the lifecycle's stop funcs are
 // called.
 // If any of the fns fail to run and returns an error, the app exits with exit code
 // 1.
-// If none of the fns are blocking, use Run instead.
 func (a *App) Start(fns ...Runner) {
-	osInt := make(chan os.Signal, 1)
-
-	signal.Notify(osInt, syscall.SIGINT, syscall.SIGTERM)
-
-	go func() {
-		<-osInt
-		a.Lifecycle.Stop()
-	}()
-
 	for i := range fns {
 		err := fns[i].Run()
 		if err != nil {
@@ -76,4 +65,12 @@ func (a *App) Start(fns ...Runner) {
 			os.Exit(1)
 		}
 	}
+
+	osInt := make(chan os.Signal, 1)
+
+	signal.Notify(osInt, syscall.SIGINT, syscall.SIGTERM)
+
+	<-osInt
+
+	a.Lifecycle.Stop()
 }
