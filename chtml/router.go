5d9eeab8ad7e5ada190a9fd7a338a70e3258cd7b
Tushar Soni <tusharsoni1205@gmail.com>
2021-09-04 21:29:58 -0400

    Add ability to configure SPA routing in chtml

--- chtml/router.go
+++ chtml/router.go
@@ -1,48 +1,56 @@
 package chtml
 
 import (
-	"io/fs"
-	"net/http"
-
 	"github.com/gocopper/copper/chttp"
+	"net/http"
 )
 
 type (
 	// Router provides routes to serve static assets and the index page for a single-page app
 	Router struct {
-		dir fs.FS
-		rw  *ReaderWriter
+		rw     *ReaderWriter
+		config Config
+
+		staticFileServer http.Handler
 	}
 
 	// NewRouterParams holds the params needed to instantiate a new Router
 	NewRouterParams struct {
 		StaticDir StaticDir
 		RW        *ReaderWriter
+		Config    Config
 	}
 )
 
 // NewRouter instantiates a new Router
 func NewRouter(p NewRouterParams) *Router {
 	return &Router{
-		dir: p.StaticDir,
-		rw:  p.RW,
+		rw:     p.RW,
+		config: p.Config,
+
+		staticFileServer: http.FileServer(http.FS(p.StaticDir)),
 	}
 }
 
 // Routes defines the HTTP routes for this router
 func (ro *Router) Routes() []chttp.Route {
-	return []chttp.Route{
-		{
-			Path:    "/{path:.*}",
-			Methods: []string{http.MethodGet},
-			Handler: ro.HandleIndexPage,
-		},
+	routes := []chttp.Route{
 		{
 			Path:    "/static/{path:.*}",
 			Methods: []string{http.MethodGet},
-			Handler: http.FileServer(http.FS(ro.dir)).ServeHTTP,
+			Handler: ro.staticFileServer.ServeHTTP,
 		},
 	}
+
+	if ro.config.EnableSinglePageRouting {
+		routes = append(routes, chttp.Route{
+			Path:    "/{path:.*}",
+			Methods: []string{http.MethodGet},
+			Handler: ro.HandleIndexPage,
+		})
+	}
+
+	return routes
 }
 
 // HandleIndexPage renders the index.html page
