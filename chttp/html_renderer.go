9aac42026ab1d7abbd95fb86bce7648aa6239944
Tushar Soni <tusharsoni1205@gmail.com>
2022-05-15 23:24:18 -0400

    Remove assets from html renderer

--- chttp/html_renderer.go
+++ chttp/html_renderer.go
@@ -1,8 +1,6 @@
 package chttp
 
 import (
-	"encoding/json"
-	"fmt"
 	"html/template"
 	"io/fs"
 	"net/http"
@@ -62,7 +60,6 @@ func NewHTMLRenderer(p NewHTMLRendererParams) (*HTMLRenderer, error) {
 func (r *HTMLRenderer) funcMap(req *http.Request) template.FuncMap {
 	return template.FuncMap{
 		"partial": r.partial(req),
-		"assets":  r.assets(req),
 	}
 }
 
@@ -116,108 +113,4 @@ func (r *HTMLRenderer) partial(req *http.Request) func(name string, data interfa
 		// nolint:gosec
 		return template.HTML(dest.String()), nil
 	}
-}
-
-func (r *HTMLRenderer) assets(req *http.Request) func() (template.HTML, error) {
-	return func() (template.HTML, error) {
-		// todo: remove vite assets
-		if false {
-			return r.devAssets(req)
-		}
-
-		return r.prodAssets()
-	}
-}
-
-func (r *HTMLRenderer) devAssets(req *http.Request) (template.HTML, error) {
-	const reactRefreshURL = "http://localhost:3000/@react-refresh"
-	var out strings.Builder
-
-	reactReq, err := http.NewRequestWithContext(req.Context(), http.MethodGet, reactRefreshURL, nil)
-	if err != nil {
-		return "", cerrors.New(err, "failed to create request for @react-refresh", nil)
-	}
-
-	resp, err := http.DefaultClient.Do(reactReq)
-	if err != nil {
-		return "", cerrors.New(err, "failed to execute request for @react-refresh", nil)
-	}
-	defer func() { _ = resp.Body.Close() }()
-
-	if resp.StatusCode == http.StatusOK {
-		out.WriteString(`
-<script type="module">
-  import RefreshRuntime from 'http://localhost:3000/@react-refresh'
-  RefreshRuntime.injectIntoGlobalHook(window)
-  window.$RefreshReg$ = () => {}
-  window.$RefreshSig$ = () => (type) => type
-  window.__vite_plugin_react_preamble_installed__ = true
-</script>`)
-	}
-
-	// note: in dev mode only, the css is not part of the initial page load. since it is loaded async, there is
-	// a brief time period where the page has no styles. to avoid this, the following snippet hides the
-	// body until the css has been loaded.
-	out.WriteString(`
-<style type="text/css" id="copper-hide-body">
-	body { visibility: hidden; }
-</style>
-<script type="text/javascript">
-	(function() {
-		let interval;
-	
-		function showBodyIfStylesPresent() {
-			const styleEls = document.getElementsByTagName('style');
-			const copperHideBodyStyleEl = document.getElementById('copper-hide-body');
-			
-			if (!copperHideBodyStyleEl || styleEls.length === 1) {
-				return;
-			}
-			
-			copperHideBodyStyleEl.remove();
-			clearInterval(interval);
-		}
-	
-		interval = setInterval(showBodyIfStylesPresent, 100);
-	})();
-</script>
-`)
-
-	out.WriteString(`
-<script type="module" src="http://localhost:3000/@vite/client"></script>
-<script type="module" src="http://localhost:3000/src/main.js"></script>`)
-
-	// nolint:gosec
-	return template.HTML(out.String()), nil
-}
-
-func (r *HTMLRenderer) prodAssets() (template.HTML, error) {
-	var (
-		out      strings.Builder
-		manifest struct {
-			MainJS struct {
-				File string   `json:"file"`
-				CSS  []string `json:"css"`
-			} `json:"src/main.js"`
-		}
-	)
-
-	manifestFile, err := r.staticDir.Open("static/manifest.json")
-	if err != nil {
-		return "", cerrors.New(err, "failed to open manifest.json", nil)
-	}
-
-	err = json.NewDecoder(manifestFile).Decode(&manifest)
-	if err != nil {
-		return "", cerrors.New(err, "failed to decode manifest.json", nil)
-	}
-
-	if len(manifest.MainJS.CSS) == 1 {
-		out.WriteString(fmt.Sprintf("<link rel=\"stylesheet\" href=\"/static/%s\" />\n", manifest.MainJS.CSS[0]))
-	}
-
-	out.WriteString(fmt.Sprintf("<script type=\"module\" src=\"/static/%s\"></script>", manifest.MainJS.File))
-
-	//nolint:gosec
-	return template.HTML(out.String()), nil
 }
