d36ceee2f7ee5173b4ab0e4caabb0a3548b90e5c
Tushar Soni <tusharsoni1205@gmail.com>
2023-08-16 21:34:12 -0700

    Add request id middleware to chttp

---
+++ chttp/request_id_mw.go
@@ -0,0 +1,37 @@
+package chttp
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/google/uuid"
+)
+
+type ctxRequestID string
+
+const ctxRequestIDKey = ctxRequestID("chttp/request-id")
+
+// SetRequestIDInCtxMiddleware sets a unique request id in the context
+func SetRequestIDInCtxMiddleware() Middleware {
+	var mw = func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			ctx := context.WithValue(r.Context(), ctxRequestIDKey, uuid.New().String())
+
+			next.ServeHTTP(w, r.WithContext(ctx))
+		})
+	}
+
+	return HandleMiddleware(mw)
+}
+
+// GetRequestID returns the request id from the context.
+// If the request id is not found in the context, it panics.
+// It must be used only after the request id middleware is applied.
+func GetRequestID(ctx context.Context) string {
+	requestID, ok := ctx.Value(ctxRequestIDKey).(string)
+	if !ok || requestID == "" {
+		panic("request id not found in context")
+	}
+
+	return requestID
+}
